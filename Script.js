9f35e88ec3983cc42e2d7b7a7ac66ca52bc3f801
anirban185 <phukonanirban7@gmail.com>
2026-08-13 07:52:01 +0530

    Add JS to switch from menu to game screen

---
+++ Script.js
@@ -0,0 +1,18 @@
+let selectLanguage = null;
+
+const filenames = {
+    javascript: 'editor.js',
+    python: 'editor.py',
+    'c++': 'editor.cpp',
+    java: 'editor.java'
+};
+
+function startGame(lang) {
+    selectLanguage = lang;
+
+    document.getElementById('menuScreen').classList.add('hidden');
+    document.getElementById('topbar').classList.remove('hidden');
+    document.getElementById('gameContainer').classList.remove('hiddem');
+
+    document.getElementById('editorFilename').textContent = filenames[lang];
+}
