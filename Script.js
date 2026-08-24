a55b2e2f342b7243bfa8f026e8c9954b71e5785e
anirban185 <phukonanirban7@gmail.com>
2026-08-24 22:51:12 +0530

    Fix puzzle shuffle

--- Script.js
+++ Script.js
@@ -279,7 +279,7 @@ function shuffleArray(arr) {
     for (let i = shuffled.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         const temp = shuffled[i];
-        shuffled[i] = shuffled[i];
+        shuffled[i] = shuffled[j];
         shuffled[j] = temp;
     }
 
